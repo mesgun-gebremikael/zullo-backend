cced9ae9675c416d4d4deb8d5928486335bc6d8e
Mesgun <Mesgun@MESGUN->
2026-02-06 04:42:55 +0100

    mm

--- Program.cs
+++ Program.cs
@@ -53,7 +53,7 @@ namespace Zullo.Api
 
             app.UseHttpsRedirection();
 
-            // You can keep this even if we don't use it yet ska raderas sedan om vi inte behöver det
+            // You can keep this even if we don't use it yet ska raderas sedan om vi inte behöver det 
             app.UseAuthorization();
 
             app.MapControllers();
